4c8052b9837c748222dd88308e27f134b1c4724c
Theo <theo@theo.140.233.243>
2025-11-27 22:36:40 +0700

    add javascript

---
+++ script.js
@@ -0,0 +1,51 @@
+const photos = [
+    "img/gal/all photos/1.png",
+    "img/gal/all photos/2.png",
+    "img/gal/all photos/3.png",
+    "img/gal/all photos/4.png",
+    "img/gal/all photos/5.png",
+    "img/gal/all photos/6.png",
+    "img/gal/all photos/7.png",
+    "img/gal/all photos/8.png",
+    "img/gal/all photos/9.png",
+    // {src: "img/gal/all photos/10.png"},
+]
+
+function removePhotos(number){
+    for(let i = 1; i <= number; i++){
+        const image1 = document.getElementById('img-' + i);
+        image1.parentNode.removeChild(image1);
+    }
+}
+
+function displayGallery(){
+    const box = document.getElementById('box');
+    const date = document.getElementById('date');
+    const click = document.getElementById('click-here');
+
+    click.style.display = 'none';
+    box.style.display = 'initial';
+    box.style.opacity = '1';
+    box.style.transition = '0.5s ease';
+    date.style.display = 'initial';
+    date.style.opacity = '1';
+    date.style.transition = '0.5s ease';
+}
+
+function displayPhotos(display){
+    let source = "img/gal/all_photos/";
+    // hide image
+    for(let i = 10; i > display; i--){
+        const image = document.getElementById('img-' + i);
+        image.style.display = 'none';
+    }
+
+    // show image
+    for(let i = 1; i <= display; i++){
+        const image = document.getElementById('img-' + i);
+        image.style.display = 'initial';
+        image.style.opacity = '1';
+        image.style.transition = '0.5s ease';
+        // requestAnimationFrame(() => image.classList.add("show"));
+    }
+}
